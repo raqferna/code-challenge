a0de3cbb712f421605f990d88468e04fc08d0a48
raquel <47450087+raqferna@users.noreply.github.com>
2022-03-28 10:41:00 +0200

    Update FilePortController.java

--- seat-challenge-code/src/main/java/com/seatchallenge/mower/infrastructure/FilePortController.java
+++ seat-challenge-code/src/main/java/com/seatchallenge/mower/infrastructure/FilePortController.java
@@ -8,10 +8,18 @@ public class FilePortController {
 	private static final String CSV_OUT_PATH="mowersOutput.csv";
 	private static final String CSV_PATH="mowers.csv";
 	
-	 public static void main(String[] args) throws NumberFormatException, IOException {
+        public static void main(String[] args) throws NumberFormatException, IOException {
 		 PlateuController controller = new PlateuController();
-		 InputFilePort.readFile(CSV_PATH, controller);
+		 InputFilePort.readFile(getFile(args), controller);
 		 OputputFilePort.writeFile(controller.getMowersPositions(), CSV_OUT_PATH);
 	 }
+	 
+	 private static String getFile(String[]args) {
+		 if(args !=null && args[0]!= null) {
+			 return args[0];
+		 }
+		return  CSV_PATH;
+	 }
+	
 
 }
